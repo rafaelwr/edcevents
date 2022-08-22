8c98abe14309323fd86b493f3b5276e1782411b0
Teste <teste@saleplus.net.br>
2022-08-22 19:27:05 -0300

    Criada a funcionalidade de remocao do evento.

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -24,7 +24,14 @@
                         <td scope="row">{{ $loop->index + 1 }}</td>
                         <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
                         <td>0</td>
-                        <td><a href="#">Editar</a> <a href="#">Excluir</a></td>
+                        <td>
+                            <a href="#" class="btn btn-info edit-btn"><ion-icon name="create-outline"></ion-icon> Editar</a>
+                            <form action="/events/{{ $event->id }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger delete-btn"><ion-icon name="trash-outline"></ion-icon> Excluir</button>
+                            </form>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
