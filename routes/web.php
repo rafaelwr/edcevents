0f68ae65bf1449bc3aee0e56040d3fad5105bf29
Teste <teste@saleplus.net.br>
2022-08-22 19:11:03 -0300

    Criada a pagina de dashboard

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,5 @@ Route::post('/events', [EventController::class, 'store']);
 Route::get('/contact', function() {
     return view('contact');
 });
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
