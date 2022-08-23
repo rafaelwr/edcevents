27fc603b95a5776ff553d1e594303724c3bff09d
Teste <teste@saleplus.net.br>
2022-08-22 22:24:31 -0300

    Criada a funcionalidade de sair do evento.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -69,9 +69,24 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if($user){
+
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach($userEvents as $userEvent){
+                if($userEvent['id'] == $id){
+                    $hasUserJoined = true;
+                }
+            }
+
+        }
+
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
 
     }
 
@@ -143,6 +158,18 @@ class EventController extends Controller
         $event = Event::findOrFail($id);
 
         return redirect('/dashboard')->with('msg', 'Sua participação está confirmada no evento ' . $event->title);
+
+    }
+
+    public function leaveEvent($id){
+
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->detach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Sua participação foi removida do evento ' . $event->title);
 
     }
 
