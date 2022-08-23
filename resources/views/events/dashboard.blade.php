f3b8fbe9caec55cdd0c5e5313a7eb86785a6261a
Teste <teste@saleplus.net.br>
2022-08-22 21:56:14 -0300

    Criada a listagem de eventos que usuario participa.

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -40,5 +40,36 @@
         <p>Você ainda não tem eventos. <a href="/events/create">Criar Evento!</a></p>
     @endif
 </div>
+<div class="col-md-10 offset-md-1 dashboard-title-container">
+    <h1>Eventos que estou participando</h1>
+</div>
+<div class="col-md-10 offset-md-1 dashboard-events-container">
+    @if(count($eventsAsParticipant) > 0)
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Participantes</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($eventsAsParticipant as $event)
+                    <tr>
+                        <td scope="row">{{ $loop->index + 1 }}</td>
+                        <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
+                        <td>{{ count($event->users) }}</td>
+                        <td>
+                            <a href="#">Sair do Evento</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>Você não está participando de nenhum evento. <a href="/">Clique aqui para ver os eventos.</a></p>
+    @endif
+</div>
 
 @endsection
